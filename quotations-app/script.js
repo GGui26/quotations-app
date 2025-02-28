6b0922d519bac524ff84b4c495db785bb3e21233
GGui26 <gui.alexandre.fernandes.ca@gmail.com>
2025-02-28 13:25:45 +0100

    main

--- quotations-app/script.js
+++ quotations-app/script.js
@@ -1,9 +1,28 @@
-const buttonSubmit = document.getElementById('submit-button')
+const form = document.getElementById('form')
 const authorName = document.getElementById('author-name')
 const citation = document.getElementById('text')
 
+function submitCitation(authorName,citation){
+    addquote(authorName,citation)}
 
-buttonSubmit.addEventListener('click',submit)
+buttonSubmit.addEventListener('click',submitCitation)
 
-function submit(authorName,citation){
-    console.log (authorName,citation)}
+
+function addquote(quote,author){}
+const liste = document.getElementById('quote-list')
+const newdiv =document.createElement('div')
+newdiv.className = "quote"
+liste.appendChild(newdiv)
+
+const authorname =document.createElement('p')
+author.classList.add ("author")
+newdiv.appendChild(author)
+
+const text =document.createElement('p')
+text.classList.add ("text")
+newdiv.appendChild(text)
+newdiv.document.getElementById("text").textContent = quote
+author.document.getElementById("text").textContent = author
+
+
+
